3c0946b061f0ea185cbf7612e37a42f4c852c905
AlexBarbosa169 <alexsrb169@hotmail.com>
2018-09-19 15:26:47 -0300

    Bread Crumb e links para navegação entre páginas

--- lib.php
+++ lib.php
@@ -13,3 +13,44 @@ function funcao_teste(){
         }
       </script>";
 }
+
+// função para impressão do bread crumb e as páginas referentes a cada solicitação do usuário
+function print_navigation_evasionview($params,$url ,$OUTPUT) {
+//        Incluir aqui o que se espera quando o usuário requer a visualização do grupo de risco dos usuários
+        if($params['group']){            
+            print_icon_evasionview('group');
+            echo $OUTPUT->action_link(new moodle_url($url, array('userinfo'=>3)), "Navegar para usuário");
+        }else{
+//        Incluir aqui o que se espera quando o usuário requer a visualização das informações sobre um usuário em específico
+            if($params['userinfo']){
+            print_icon_evasionview('userinfo');  
+            echo $OUTPUT->action_link(new moodle_url($url, array('usersend'=>3)), "Navegar para enviar mensagem");            
+            }else{
+//        Incluir aqui o que se espera quando o usuário requer notificar um usuário selecionado no grupo de risco
+                if($params['usersend']){
+                print_icon_evasionview('usersend');                
+                echo $OUTPUT->action_link(new moodle_url($url),"Navegar para Home");            
+                }else{
+//        Incluir aqui o que se espera quando o usuário entra no link principal do plugin
+                    print_icon_evasionview('home');                    
+                    echo $OUTPUT->action_link(new moodle_url($url,array('group'=>3)),"Navegar para Grupo de Usuários");
+                }            
+            }
+        }    
+}
+
+function print_icon_evasionview($param) {
+    $breadcrumb = array('home','group','userinfo','usersend');        
+    echo "<div class='nav-evasion'>";
+    foreach ($breadcrumb as $key => $value){
+        if($param != $value){
+            $opacity = "style='opacity:0.1'";
+        }else{
+            $opacity = "";
+        }
+     echo "<div id='nav-icon' $opacity>
+            <img class='bread' src='img/$value.png' alt='global'/>
+           </div>";   
+    }    
+    echo "</div>";
+}
